f710b386b87b249ed2a1628276c538cf1cfa4f55
velaspis <91197854+velaspis@users.noreply.github.com>
2023-01-29 14:22:00 +0100

    promises

---
+++ promises/ans.js
@@ -0,0 +1,115 @@
+export class TranslationService {
+    /**
+     * Creates a new service
+     * @param {ExternalApi} api the original api
+     */
+    constructor(api) {
+      this.api = api;
+    }
+    /**
+     * Attempts to retrieve the translation for the given text.
+     *
+     * - Returns whichever translation can be retrieved, regardless the quality
+     * - Forwards any error from the translation api
+     *
+     * @param {string} text
+     * @returns {Promise<string>}
+     */
+    free(text) {
+      return this.api.fetch(text).then((result) => result.translation)
+    }
+    /**
+     * Batch translates the given texts using the free service.
+     *
+     * - Resolves all the translations (in the same order), if they all succeed
+     * - Rejects with the first error that is encountered
+     * - Rejects with a BatchIsEmpty error if no texts are given
+     *
+     * @param {string[]} texts
+     * @returns {Promise<string[]>}
+     */
+    batch(texts) {
+      if (texts.length === 0) {
+        return Promise.reject(new BatchIsEmpty())
+      }
+      
+      return Promise.all(texts.map(this.free.bind(this)))
+    }
+    /**
+     * Requests the service for some text to be translated.
+     *
+     * Note: the request service is flaky, and it may take up to three times for
+     *       it to accept the request.
+     *
+     * @param {string} text
+     * @returns {Promise<void>}
+     */
+    request(text) {
+      // This is a function that will return a promise. The promise
+      // requests from the API a translation, and uses its callback
+      // to resolve or reject the promise.
+      const promisify = () => new Promise((resolve, reject) => {
+        this.api.request(text, (result) => {
+          result ? reject(result) : resolve();
+        })
+      })
+      
+      return promisify()  //   try
+        .catch(promisify) // retry one
+        .catch(promisify) // retry two
+    }
+    /**
+     * Retrieves the translation for the given text
+     *
+     * - Rejects with an error if the quality can not be met
+     * - Requests a translation if the translation is not available, then retries
+     *
+     * @param {string} text
+     * @param {number} minimumQuality
+     * @returns {Promise<string>}
+     */
+    premium(text, minimumQuality) {
+      return this.api.fetch(text)
+        .catch(() => {
+          // When it fails to fetch, request it.
+          // When the request passes, fetch it again.
+          return this.request(text).then(() => this.api.fetch(text))
+        })
+        .then((result) => {
+          if (result.quality < minimumQuality) {
+            throw new QualityThresholdNotMet()
+          }
+          return result.translation
+        })
+    }
+  }
+  /**
+   * This error is used to indicate a translation was found, but its quality does
+   * not meet a certain threshold. Do not change the name of this error.
+   */
+  export class QualityThresholdNotMet extends Error {
+    /**
+     * @param {string} text
+     */
+    constructor(text) {
+      super(
+        `
+  The translation of ${text} does not meet the requested quality threshold.
+      `.trim()
+      );
+      this.text = text;
+    }
+  }
+  /**
+   * This error is used to indicate the batch service was called without any
+   * texts to translate (it was empty). Do not change the name of this error.
+   */
+  export class BatchIsEmpty extends Error {
+    constructor() {
+      super(
+        `
+  Requested a batch translation, but there are no texts in the batch.
+      `.trim()
+      );
+    }
+  }
